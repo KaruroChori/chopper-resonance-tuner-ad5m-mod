f25acf1755fe8ebcf78afe0982e49fc0da26fc13
KaruroChori <KaruroChori@users.noreply.github.com>
2024-05-06 14:12:27 +0000

    Update chopper_plot.sh

--- chopper_plot.sh
+++ chopper_plot.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-script_path=$(realpath $(echo $0))
-repo_path=$(dirname ${script_path})
-source ${repo_path}/.venv/bin/activate
-exec python3 ${repo_path}/chopper_plot.py "$@"
+#!/bin/sh
+#script_path=$(realpath $(echo $0))
+#repo_path=$(dirname ${script_path})
+#source ${repo_path}/.venv/bin/activate
+exec python3 /root/chopper-resonance-tuner-ad5m-mod/chopper_plot.py "$@"
